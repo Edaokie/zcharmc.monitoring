51af0f5cae3e648f0feb1b70f0a961359f83fb96
Edrian Abilay <edoaki@Edrians-MacBook-Pro.local>
2026-06-29 16:53:36 +0800

    restructure: organize into firmware, backend, frontend folder sir paul

---
+++ firmware/include/secrets.h
@@ -0,0 +1,23 @@
+// #ifndef SECRETS_EXAMPLE_H
+// #define SECRETS_EXAMPLE_H
+
+// constexpr const char* WIFI_SSID = "YOUR_WIFI_NAME";
+// constexpr const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+// constexpr const char* MQTT_USERNAME = "YOUR_MQTT_USERNAME";
+// constexpr const char* MQTT_PASSWORD = "YOUR_MQTT_PASSWORD";
+
+// constexpr const char* API_KEY = "YOUR_API_KEY";
+
+// #endif
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define WIFI_SSID       "MSCA"
+#define WIFI_PASSWORD   "micel2k21"
+
+// ThingsBoard access token
+#define TB_ACCESS_TOKEN "zM0uLDT57nm8t2EihVkz"
+
+#endif
